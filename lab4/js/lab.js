b56c5e5cfbc30ad03488203e27c566038c4111ea
sophiejhan <sophie.han13@gmail.com>
2022-10-12 20:39:56 -0700

    Lab 4 index html was created and also css.

---
+++ lab4/js/lab.js
@@ -0,0 +1,15 @@
+// Lab 4.
+// Pseudocode for an online checkers game.
+// @link URL
+// @file This files defines the online checkers game.
+// @author SophieHan KazarinaLiao.
+// @since  10.12.2022
+
+// Creating a checkers game:
+  // Print game board before each turn.
+  // Invite two players.
+  // Accept player 1 input.
+  // Validate player input.
+  // Accept player 2 input.
+  // Validate player 2 input.
+  // Test win or draw condition.
