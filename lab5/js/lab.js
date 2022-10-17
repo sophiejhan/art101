4d0e7c5d07a9d867b4cea13c7ea732acef200efc
sophiejhan <sophie.han13@gmail.com>
2022-10-17 11:18:45 -0700

    Completed Lab 5 tasks

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+// Authors: Sophie Han <sojhan@ucsc.edu> Kazarina Liao  <kmliao@ucsc.edu>
+// Created: 17 October 2022
+// License: Public Domain
+
+
+// Define Variables
+make= "Toyota";
+model= "Corola";
+color= "White";
+year= "2020";
+
+//calculate
+age=2022-year;
+
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
