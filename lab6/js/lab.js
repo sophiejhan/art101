c9bce25ea52437459e8de59e51d4b9ec4f63f2d8
sophiejhan <sophie.han13@gmail.com>
2022-10-19 11:25:49 -0700

    Lab 6 Completed. Declared arrays and objects in JS then created a Lab 6 index.html

---
+++ lab6/js/lab.js
@@ -0,0 +1,19 @@
+// Authors: Sophie Han <sojhan@ucsc.edu> Kazarina Liao  <kmliao@ucsc.edu>
+// Created: 19 October 2022
+// License: Public Domain
+
+myTransport = ["walking", "biking", "driving"];
+
+myMainRide = {
+    make: "Toyota",
+    model: "Corolla",
+    color: "White",
+    year: 2020,
+    age : function() {
+        return 2019 - age;
+    }
+}
+
+document.writeln("Kinds of transport I use: ", myTransport, "</br>");
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
