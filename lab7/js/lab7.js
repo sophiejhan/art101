3bb0f72339a28476b31b9f59cf1d57f3b2c701e3
sophiejhan <sophie.han13@gmail.com>
2022-10-24 11:15:42 -0700

    Lab7 completed

---
+++ lab7/js/lab7.js
@@ -0,0 +1,23 @@
+// Authors: Sophie Han <sojhan@ucsc.edu> Kazarina Liao  <kmliao@ucsc.edu>
+// Created: 24 October 2022
+// License: Public Domain
+
+function sortUserName()  {
+    var userName = window.prompt ("Hello! Please type your username here.");
+    console.log("userName =", userName);
+    //split string to array
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArraySort);
+    //sort the array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+    //join array back to a string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+
+    return nameSorted;
+}
+
+//output
+document.writeln("Hey! Nice username: ",
+    sortUserName(), "</br>");
