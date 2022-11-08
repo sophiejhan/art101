2a3ca0e3207af64d7f227a3df5ed4a5893bb322a
sophiejhan <sophie.han13@gmail.com>
2022-11-07 19:28:56 -0800

    Lab11 with css changes and bonus task!

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,5 +1,5 @@
 // Authors: Sophie Han <sojhan@ucsc.edu> Kazarina Liao  <kmliao@ucsc.edu>
-// Created: 2 November 2022
+// Created: 7 November 2022
 // License Public Domain
 
 $('#challenges').append('<button id="buttonChalEl">Click me!</button>');
@@ -9,13 +9,27 @@ $('#problems').append('<button id="buttonProbEl">Hello!</button>');
 $('#results').append('<button id="buttonResEl">Click here!</button>');
 
 $('#buttonChalEl').click (function(){
-  $('#challenges').toggleClass("special");
+  $('#challenges').toggleClass("specialChal");
 });
 
 $('#buttonProbEl').click(function(){
-  $('#problems').toggleClass("special");
+  $('#problems').toggleClass("specialProb");
 });
 
 $('#buttonResEl').click(function(){
-  $('#results').toggleClass("special");
+  $('#results').toggleClass("specialRes");
+});
+
+
+//bonus task
+//create a button that changes the other botton's color and vice versa!
+$('#bonus').append('<button id ="buttonOneEl">I am button one! </button>');
+$('#bonus').append('<button id ="buttonTwoEl">I am button two!</button>');
+
+$('#buttonOneEl').click(function(){
+  $('#buttonTwoEl').toggleClass("bonusSpecial");
+});
+
+$('#buttonTwoEl').click(function(){
+  $('#buttonOneEl').toggleClass("bonusSpecial");
 });
