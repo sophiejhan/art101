cca854d75348838bf04bda447d87154a454b81a5
sophiejhan <sophie.han13@gmail.com>
2022-11-09 20:55:22 -0800

    lab12

---
+++ lab12/js/lab.js
@@ -0,0 +1,30 @@
+// Authors: Sophie Han <sojhan@ucsc.edu> Kazarina Liao  <kmliao@ucsc.edu>
+// Created: 9 November 2022
+// License Public Domain
+
+
+function sortingHat(str)  {
+  len = str.length;
+  mod = len % 4;
+  if (mod == 0) {
+    return "Gryffindor"
+  }
+  else if (mod == 1) {
+    return "Ravenclaw"
+  }
+  else if (mod == 2) {
+    return "Slytherin"
+  }
+  else if (mod == 3) {
+    return "Hufflepuff"
+  }
+}
+
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+    var name = document.getElementById("input").value;
+    var house = sortingHat(name);
+    newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+    document.getElementById("output").innerHTML = newText;
+})
